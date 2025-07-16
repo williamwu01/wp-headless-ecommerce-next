478ca4bd19e1203bcc5f5d44095cd0569eb2aae1
Ritesh <riteshmaharjan07@gmail.com>
2025-07-15 20:00:43 -0700

    Hovered

--- app/components/ProductCollection.tsx
+++ app/components/ProductCollection.tsx
@@ -1,16 +1,28 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 
 const ProductCollection = ({ data }) => {
+  const [currentHover, setCurrentHover] = useState(1);
+
   return (
-    <section className="flex m-auto min-h-screen">
+    <section className="relative flex m-auto items-end w-screen p-4">
       {data.map((el, i) => (
-        <div className={``} key={el.id}>
-          <h2>{el.name}</h2>
+        <div
+          className={`transition-all duration-500 ease-in-out ${currentHover === i ? "grow-[2]" : "grow-1"}`}
+          key={el.id}
+          onMouseEnter={() => setCurrentHover(i)}
+        >
+          <h2
+            className={`absolute left-4 ${
+              currentHover === i ? "block" : "hidden"
+            }`}
+          >
+            {el.name}
+          </h2>
           {/* need to useeffect to set an active class and usestate to set the 1st child to be on active before anything is hovered  */}
-          <div className="h-96 object-cover relative transition-transform duration-500 hover:scale-x-200">
-            <Image src={el.image.src} alt={el.image.alt} fill className="" />
+          <div className="mt-12 h-96 relative">
+            <Image src={el.image.src} alt={el.image.alt} fill />
           </div>
         </div>
       ))}
